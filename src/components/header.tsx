9fa017cd9236ec5b72a34da02318ffdcc22e634e
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-08 21:55:27 +0200

    commit textArea

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export function Header(){
+    return(
+        <div>
+        <h3>Home</h3>
+        </div>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,24 @@
-export function Home(){
-    return(
-        <h1>This is home </h1>
-    )
+import { Header } from "../components/header";
+
+export function Home() {
+  return (
+    <section className="home">
+      <Header />
+      <div className="user-tweet">
+        <img
+          src="https://images.pexels.com/photos/51312/kiwi-fruit-vitamins-healthy-eating-51312.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="user-icon"
+        ></img>
+        <div>
+          <form>
+            <textarea
+              placeholder="What's happening?"
+              className="tweet-input"
+            ></textarea>
+            <button className="tweet-input-btn">Tweet</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
