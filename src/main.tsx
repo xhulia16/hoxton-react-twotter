88c7f3a1abaf4a6db20db090007bb1b6cf3662c6
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-08 17:18:56 +0200

    commit routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <BrowserRouter> 
     <App />
-  </React.StrictMode>
+  </BrowserRouter> 
 )
